6cca49cd91db4e48c1cb2c8237992ae48b55fad6
Matthew Davidson <mdavidson982@g.rwu.edu>
2021-10-07 15:24:09 -0400

    Changes

--- server.js
+++ server.js
@@ -16,13 +16,35 @@
  'use strict';
 
  // [START app]
+ 
  const express = require('express');
  const app = express();
+ const path = require('path');
+ const bodyParser = require('body-parser');
  
+// [START enable_parser]
+app.use(bodyParser.urlencoded({ extended: true }));
+// [END enable_parser]
+
  app.get('/', (req, res) => {
    res.send('Hello from App Engine!');
  });
- 
+
+ // [START add_display_form]
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/www/index.html'));
+});
+// [END add_display_form]
+
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
+});
+
  // Listen to the App Engine-specified port, or 8080 otherwise
  const PORT = process.env.PORT || 8080;
  app.listen(PORT, () => {
